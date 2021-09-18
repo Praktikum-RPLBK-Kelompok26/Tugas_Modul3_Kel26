4f0977aefe76ad41214dbb9ae7175e87167883df
Ayundamita <48998872+Ayundamita@users.noreply.github.com>
2021-09-18 11:57:10 +0700

    bismillah kelar

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Parent from "./tugasmodul3/Parent";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Tugas Modul 3 Kelompok 26</h2>
+        <h3>React State and Lifecycle</h3>
+        <Parent />
+      </div>
+    )
+  }
+}
+
+export default App;
